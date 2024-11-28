85ed4603acad3c45d8fb4a7d426f5b6c1fcc3aab
JosiahFu <josiahfu@gmail.com>
2024-11-28 11:42:47 -0800

    Improve mixin

--- src/main/java/archives/tater/bundlescroll/mixin/FeatureFlagsMixin.java
+++ src/main/java/archives/tater/bundlescroll/mixin/FeatureFlagsMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.resource.featuretoggle.FeatureFlag;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.resource.featuretoggle.FeatureSet;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,7 +16,11 @@ public class FeatureFlagsMixin {
 
     @ModifyExpressionValue(
             method = "<clinit>",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/featuretoggle/FeatureSet;of(Lnet/minecraft/resource/featuretoggle/FeatureFlag;)Lnet/minecraft/resource/featuretoggle/FeatureSet;")
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/resource/featuretoggle/FeatureFlags;VANILLA_FEATURES:Lnet/minecraft/resource/featuretoggle/FeatureSet;",
+                    opcode = Opcodes.GETSTATIC
+            )
     )
     private static FeatureSet addBundles(FeatureSet original) {
         return original.combine(FeatureSet.of(BUNDLE));
